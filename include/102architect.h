1810aa65436a38fdc83f1dd043a8cafa4e44f427
Thomas Jouanolle <tjouanolle@localhost.localdomain>
2019-11-20 11:45:59 +0100

    Add Header

---
+++ include/102architect.h
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2019
+** 102architect_2019
+** File description:
+** Header for 102architect
+*/
+
+#ifndef ARCHITECT_H
+#define ARCHITECT_H
+
+#include "my.h"
+
+typedef struct matrix_3_3 {
+    double **matrix;
+    unsigned int height;
+    unsigned int width;
+    char type;
+} matrix_3_3_t;
+
+#endif
