fba9859a3dfeb8addc839e70e4289250eee4a324
RACAUD Simon <simonracaud@gmail.com>
2019-11-20 11:43:09 +0100

    Add my_put_double

--- lib/my/my_put_double.c
+++ lib/my/my_put_double.c
@@ -7,22 +7,45 @@
 
 #include "my.h"
 
-static void round_nbr(double *nbr)
+static void display_decimal_digits(double decimal, int *decimal_len,
+int nb_decimal)
 {
-    if ((int)*nbr % 10 >= 5)
-        *nbr += 10;
-    *nbr /= 10;
+    if (decimal < 10 && decimal != 0) {
+        my_putchar('0');
+        (*decimal_len)++;
+    }
+    if (decimal != 0)
+        my_put_nbr(decimal);
+    for (int i = (nb_decimal - *decimal_len); i > 0; i--)
+        my_putchar('0');
+}
+
+static double calc_decimal(double nb, int nb_decimal)
+{
+    double integer = floor(nb);
+    double decimal;
+
+    if (nb < 0)
+        integer = ceil(nb);
+    decimal = round( (nb - integer) * pow(10, nb_decimal) );
+    if (decimal < 0)
+        decimal = -decimal;
+    if (decimal == 100)
+        decimal = -1;
+    return decimal;
 }
 
 void my_put_double(double nb, int nb_decimal)
 {
-    int integer = nb;
-    double decimal = (float)(nb - (float)integer) * POW(10, nb_decimal + 1);
+    double decimal = calc_decimal(nb, nb_decimal);
     int decimal_len = 0;
 
-    if (decimal < 0)
-        decimal = -decimal;
-    my_put_nbr((int)nb);
+    if (nb < 0 && nb > -1)
+        my_putchar('-');
+    if (decimal == -1)
+        my_put_nbr(round(nb));
+    else
+        my_put_nbr(nb);
     my_putchar('.');
     if (decimal < 1) {
         decimal_len = 0;
@@ -30,9 +53,7 @@ void my_put_double(double nb, int nb_decimal)
         while (decimal / my_compute_power_rec(10, decimal_len) >= 1)
             decimal_len++;
     }
-    round_nbr(&decimal);
-    if (decimal != 0)
-        my_put_nbr(decimal);
-    for (int i = (nb_decimal - decimal_len); i > 0; i--)
-        my_putchar('0');
+    if (decimal == -1)
+        decimal = 0;
+    display_decimal_digits(decimal, &decimal_len, nb_decimal);
 }
